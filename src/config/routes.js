a385d560cdf7d78e0923f2fe38ecec38ebaceb78
matiasmdev <main.mmarquez@gmail.com>
2019-03-25 19:24:49 -0300

    Added const "paths" with all the paths of the app.

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,21 +1,71 @@
 import Home from "../main/views/Home";
-import StudentList from "../main/views/Students/StudentList";
-import StudentCreate from "../main/views/Students/StudentCreate";
 
-export const privateRoutes = {
-	Home: {
-		component: Home,
-		path: "/",
-		key: "home"
-	},
-	StudentList: {
-		component: StudentList,
-		path: "/alumnos/listar",
-		key: "student-list"
-	},
-	StudentCreate: {
-		component: StudentCreate,
-		path: "/alumnos/alta",
-		key: "student-create"
-	}
+import StudentCreate from "../main/views/students/StudentCreate";
+import StudentEdit from "../main/views/students/StudentEdit";
+import StudentList from "../main/views/students/StudentList";
+
+import CourseCreate from "../main/views/courses/CourseCreate";
+import CourseEdit from "../main/views/courses/CourseEdit";
+import CourseList from "../main/views/courses/CourseList";
+
+import CourseCategoryCreate from "../main/views/courses/category/CategoryCreate";
+import CourseCategoryEdit from "../main/views/courses/category/CategoryEdit";
+import CourseCategoryList from "../main/views/courses/category/CategoryList";
+
+export const paths = {
+	home: "/",
+	studentList: "/alumnos/listar",
+	studentCreate: "/alumnos/alta",
+	studentEdit: "/alumnos/editar/:id",
+	courseList: "/cursos/listar",
+	courseCreate: "/cursos/alta",
+	courseEdit: "/cursos/editar/:id",
+	courseCategoryList: "/cursos-categorias/listar",
+	courseCategoryCreate: "/cursos-categorias/alta",
+	courseCategoryEdit: "/cursos-categorias/editar/:id"
+};
+
+export const routes = {
+	private: [
+		{
+			path: paths.home,
+			component: Home
+		},
+		{
+			path: paths.studentList,
+			component: StudentList
+		},
+		{
+			path: paths.studentCreate,
+			component: StudentCreate
+		},
+		{
+			path: paths.studentEdit,
+			component: StudentEdit
+		},
+		{
+			path: paths.courseList,
+			component: CourseList
+		},
+		{
+			path: paths.courseCreate,
+			component: CourseCreate
+		},
+		{
+			path: paths.courseEdit,
+			component: CourseEdit
+		},
+		{
+			path: paths.courseCategoryList,
+			component: CourseCategoryList
+		},
+		{
+			path: paths.courseCategoryCreate,
+			component: CourseCategoryCreate
+		},
+		{
+			path: paths.courseCategoryEdit,
+			component: CourseCategoryEdit
+		}
+	]
 };
