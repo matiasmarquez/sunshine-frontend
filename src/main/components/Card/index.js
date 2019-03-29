2a0ab5d1ac1878992cd1165abc627bd091789d13
matiasmdev <main.mmarquez@gmail.com>
2019-03-28 21:38:52 -0300

    Added header to the card.

--- src/main/components/Card/index.js
+++ src/main/components/Card/index.js
@@ -1,17 +1,48 @@
 import React from "react";
 import styled from "styled-components";
 
+import Button from "../Button";
+
 const Card = props => {
-	const { children, ...rest } = props;
+	const { children, title, actions, ...rest } = props;
 
 	return (
 		<CardStyled {...rest} className="animated zoomIn">
+			{title && (
+				<CardHeader>
+					<CardTitle>{title}</CardTitle>
+					<CardActions>
+						{actions &&
+							actions.map((action, key) => (
+								<Button {...action} type="button" key={key} />
+							))}
+					</CardActions>
+				</CardHeader>
+			)}
 			<CardBody {...rest}>{children}</CardBody>
 		</CardStyled>
 	);
 };
 
 export default Card;
+
+const CardActions = styled.div``;
+
+const CardTitle = styled.h4`
+	line-height: 1.2;
+	margin: 0;
+	font-size: 18.8px;
+	font-weight: 500;
+	color: #2c2c2c;
+`;
+
+const CardHeader = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	align-items: center;
+	justify-content: space-between;
+	padding: 21px 21px 0;
+`;
 
 const CardBody = styled.div`
 	padding: ${props => {
