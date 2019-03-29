fa1a24569dbbb4d00fd6af8053bd2638d933a124
matiasmdev <main.mmarquez@gmail.com>
2019-03-28 21:43:53 -0300

    Changed structure of the component Topbar.

--- src/main/layouts/private/components/Topbar.js
+++ src/main/layouts/private/components/Topbar.js
@@ -1,37 +1,107 @@
 import React from "react";
 import styled from "styled-components";
 
+import { paths } from "config/routes";
+import StarredPages from "main/components/StarredPages";
+import FeatherIcon from "feather-icons-react";
+
 const Topbar = props => {
 	return (
-		<TopbarStyled>
-			<NavbarRight />
-		</TopbarStyled>
+		<Wrapper>
+			<Navbar>
+				<StarredPages
+					pages={[
+						{
+							path: paths.studentList,
+							text: "Alumnos",
+							icon: "users"
+						},
+						{
+							path: paths.courseList,
+							text: "Cursos",
+							icon: "book-open"
+						},
+						{
+							path: paths.staffList,
+							text: "Staff",
+							icon: "star"
+						}
+					]}
+				/>
+				<IdentityInfo>
+					<IdentityInfoContainer>
+						<IdentityName>Sheila Weksler</IdentityName>
+						<IdentityRole>Administrador</IdentityRole>
+					</IdentityInfoContainer>
+					<IdentityIconContainer>
+						<FeatherIcon icon="user" size={20} />
+					</IdentityIconContainer>
+				</IdentityInfo>
+			</Navbar>
+		</Wrapper>
 	);
 };
 
 export default Topbar;
 
-const TopbarStyled = styled.div`
-	left: 0;
-	position: fixed;
-	right: 0;
-	top: 0;
-	padding: 0 10px 0 0;
-	z-index: 999;
-	background: #fff;
-	min-height: 70px;
-	box-shadow: rgba(12, 52, 75, 0.05) 0px 3px 3px;
-	transition: all 0.3s cubic-bezier(0, 0, 0.2, 1);
-	@media (min-width: 992px) {
-		margin-left: 250px;
-	}
+const IdentityInfo = styled.div`
+	display: flex;
+	align-items: center;
 `;
 
-const NavbarRight = styled.ul`
+const IdentityInfoContainer = styled.div`
+	text-align: right;
+	margin-right: 10.5px;
+`;
+
+const IdentityName = styled.p`
+	font-weight: 600;
+	margin: 0;
+	color: #626262;
+`;
+
+const IdentityRole = styled.small`
+	color: #626262;
+`;
+
+const IdentityIconContainer = styled.div`
+	background: #efefef;
+	width: 32px;
+	height: 32px;
+	text-align: center;
+	padding: 5px;
+	border-radius: 50%;
+`;
+
+const Navbar = styled.header`
 	display: flex;
-	float: right;
-	list-style: none;
-	padding-left: 0;
-	margin-bottom: 0;
-	margin-top: 11px;
+	position: fixed;
+	align-items: center;
+	justify-content: space-between;
+	box-shadow: 0 4px 20px 0 rgba(0, 0, 0, 0.05);
+	padding: 11px 14px;
+	width: calc(100% - 320px);
+	min-height: 62px;
+	background: #fff;
+	border-radius: 7px;
+	z-index: 10000;
+`;
+
+const Wrapper = styled.div`
+	position: fixed;
+	top: 0;
+	padding: 30px 0;
+	padding-top: 18px;
+	width: calc(100% - 320px);
+	margin-left: 290px;
+	height: 103px;
+	background: linear-gradient(
+		180deg,
+		hsla(0, 0%, 97.3%, 0.95) 44%,
+		hsla(0, 0%, 97.3%, 0.46) 73%,
+		hsla(0, 0%, 100%, 0)
+	);
+	background-repeat-x: repeat;
+	background-repeat-y: no-repeat;
+	z-index: 9;
 `;
